368707c034d86b9d686e944fbc58658f608cd452
seohee <jsh001505@gmail.com>
2022-09-10 17:50:45 +0900

    더미 데이터 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,9 @@
     :rooms="JSON.stringify(rooms)"
     :messages="JSON.stringify(messages)"
     :room-actions="JSON.stringify(roomActions)"
+    :height="'calc(100vh - 5vh)'"
+    :rooms-loaded="roomsLoaded"
+    :messagesLoaded="messagesLoaded"
   />
 </template>
 
@@ -15,12 +18,60 @@ export default {
   data() {
     return {
       currentUserId: '1234',
-      rooms: [],
-      messages: [],
-      roomActions: [
-        { name: 'inviteUser', title: 'Invite User' },
-        { name: 'removeUser', title: 'Remove User' },
-        { name: 'deleteRoom', title: 'Delete Room' },
+      rooms: [
+        {
+          roomId: 1,
+          roomName: 'Room 1',
+          avatar: 'assets/imgs/people.png',
+          unreadCount: 4,
+          index: 3,
+          lastMessage: {
+            content: 'Last message received',
+            senderId: 1234,
+            username: 'John Doe',
+            timestamp: '10:20',
+            saved: true,
+            distributed: false,
+            seen: false,
+            new: true,
+          },
+          users: [
+            {
+              _id: 1234,
+              username: 'John Doe',
+              avatar: 'assets/imgs/doe.png',
+              status: {
+                state: 'online',
+                lastChanged: 'today, 14:30',
+              },
+            },
+            {
+              _id: 4321,
+              username: 'John Snow',
+              avatar: 'assets/imgs/snow.png',
+              status: {
+                state: 'offline',
+                lastChanged: '14 July, 20:00',
+              },
+            },
+          ],
+          typingUsers: [4321],
+        },
+      ],
+      roomsLoaded: true,
+      messagesLoaded: true,
+      loadingRooms: false,
+      messages: [
+        {
+          content: 'Last message received',
+          senderId: 1234,
+          username: 'John Doe',
+          timestamp: '10:20',
+          saved: true,
+          distributed: false,
+          seen: false,
+          new: true,
+        },
       ],
     };
   },
