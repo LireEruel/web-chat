0d995833fddc9dde2c02b48cb4e7bfddc6ed9f20
LireEruel <jsh001505@gmail.com>
2022-09-25 12:09:41 +0900

    vite 설정

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,24 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/web-chat/",
   plugins: [
     vue({
       template: {
         compilerOptions: {
-          isCustomElement: (tagName) => tagName === 'vue-advanced-chat',
+          isCustomElement: (tagName) => tagName === "vue-advanced-chat",
         },
       },
     }),
   ],
   resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"],
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
 });
